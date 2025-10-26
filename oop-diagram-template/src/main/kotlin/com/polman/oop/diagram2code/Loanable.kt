6d472b54a4c8d7ead704fd423d6abf344c19ff70
Rendiana Fauzi Gunawan <153361432+Rendilennon@users.noreply.github.com>
2025-10-26 18:57:11 +0700

    Delete oop-diagram-template directory
    deleting unnecessary directories

--- oop-diagram-template/src/main/kotlin/com/polman/oop/diagram2code/Loanable.kt
+++
@@ -1,7 +0,0 @@
-package com.polman.oop.diagram2code
-
-interface Loanable {
-
-    // Fungsi loan menerima parameter berupa objek Member yang melakukan peminjaman.
-    fun loan(to: Member): Boolean // Mengembalikan nilai Boolean: true jika peminjaman berhasil, false jika gagal.
-}
--- oop-diagram-template/src/main/kotlin/com/polman/oop/diagram2code/MemberLevel.kt
+++
@@ -1,8 +0,0 @@
-package com.polman.oop.diagram2code
-
-// Sebuah fungsi enum class untuk Level setiap member agar opsi dibatasi hanya ke-3 level member saja
-enum class MemberLevel {
-    REGULAR,
-    GOLD,
-    PLATINUM
-}
